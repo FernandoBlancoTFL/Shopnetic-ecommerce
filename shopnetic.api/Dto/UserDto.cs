a898f3b7277d392ff95f379b8032e3649bdfd9a1
Fernando <fernando.blanco004@gmail.com>
2025-07-26 16:46:54 -0300

    CRUD method for user created

--- shopnetic.api/Dto/UserDto.cs
+++ shopnetic.api/Dto/UserDto.cs
@@ -1,21 +1,32 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace shopnetic.api.Models
 {
-    public class User
+    public class UserDto
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public String firstName { get; set; }
+        [Required]
         public String lastName { get; set; }
+        [Required]
         public String userName { get; set; }
+        [Required]
         public String email { get; set; }
+        [Required]
         public String password { get; set; }
+        [Required]
         public String country { get; set; }
+        [Required]
         public DateTime created_at { get; set; }
+        [Required]
         public String role { get; set; }
+        [Required]
         public String image { get; set; }
     }
 }
---
+++ shopnetic.api/Migrations/20250726024529_UserModelCreated.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shopnetic.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserModelCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    firstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    lastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    userName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    image = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
